a646f31648d118ca06835a31d2b13b42b4e6cd2e
Kriaeyl <osireikri@gmail.com>
2020-06-04 16:04:53 +0800

    minor changes

--- app/src/main/java/com/myapplicationdev/android/lp3quiz1/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/lp3quiz1/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
                 NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "default");
                 builder.setContentTitle("LP3 Quiz1");
-                builder.setContentText("This is a basic/simple notification");
+                builder.setContentText("This is simple");
                 builder.setSmallIcon(android.R.drawable.btn_star_big_off);
                 builder.setContentIntent(pIntent);
                 builder.setAutoCancel(true);
